db4b1e67d7f3cbbf8e794ebd0dd6db176ae4f492
Jesus Quevedo <jenrique_qt@hotmail.com>
2022-01-19 13:54:52 -0600

    Validaciones

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query, Path
+from typing import Optional
 
 from models.Person import Person
 
@@ -14,7 +15,54 @@ def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
-#Request and responsebody
+
+# Request and responsebody
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return f"Hola {person.first_name} {person.last_name}"
+
+
+@app.get("/person/detail")
+def show_person(
+        name: Optional[str] = Query(
+            None,
+            min_length=1,
+            max_length=50,
+            title="Person Name",
+            description="This is the person name. It's between 1 and 50 chars"
+        ),
+        age: Optional[str] = Query(
+            ...,
+            min_length=1,
+            max_length=3,
+            title="Person Age",
+            description="Person age. It's required"
+        )
+):
+    return {name: age}
+
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+        person_id: int = Path(
+            ...,
+            gt=0,
+            lt=100000,
+            title="Person identifier",
+            description="This is a required element",
+        )
+):
+    return {person_id: "It exist!"}
+
+
+@app.put("/person/{person_id}")
+def show_person(
+        person_id: int = Path(
+            ...,
+            gt=0,
+            title="Person identifier",
+            description="This is a required element",
+        ),
+        person: Person = Body(...)
+):
+    return {person_id: person}
